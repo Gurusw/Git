23bd23c8b49e422814697b9eb7296c17f56bccaa
Guru <90703537+Gurusw@users.noreply.github.com>
2024-09-25 17:48:17 +0800

    Add files via upload

---
+++ AddMethod.java
@@ -0,0 +1,16 @@
+
+public class AddMethod {
+
+	static void add(int a, int b)
+	{
+		int c = a+b;
+		System.out.println("Addition of 2 values is :"+ c);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		add(1,2);
+		}
+
+}
