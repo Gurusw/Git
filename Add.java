037ecff2b0a584477ae70d33ee198ee6b28f46a1
Guru <90703537+Gurusw@users.noreply.github.com>
2024-09-25 17:05:00 +0800

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+
+public class Add {
+
+	public static void main(String[] args) {
+		int a= 23, sum=0, b=32;
+		sum = a+b;
+		if(sum>254)
+		{
+			System.out.println("Sum is Greater than 254"+ sum);
+		}
+		else
+		{
+			System.out.println("Not Greater than 254");
+		}
+	}
+
+}
