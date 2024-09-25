dd2a8b8f6d0d51a880e40228d29ba86a4c0f041f
Guru <90703537+Gurusw@users.noreply.github.com>
2024-09-25 10:18:42 +0800

    Add files via upload

---
+++ LengthOfArray.java
@@ -0,0 +1,11 @@
+
+public class LengthOfArray {
+
+	public static void main(String[] args) {
+		int []a = {1,2,4,5,6};
+		System.out.println("Lenght of an array is ="+a.length);
+		System.out.println(a[1]);
+		System.out.println(a[4]);
+	}
+
+}
